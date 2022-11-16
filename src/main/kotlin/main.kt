ecd6fe6ba9da00863488d81b280b4b6fbf71ad1a
Данила Трепачко <danhik256987014563a@gmail.com>
2022-11-16 18:43:52 +0300

    Внёс изменения в main

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -3,7 +3,9 @@ fun f(){
 }
 fun main(args: Array<String>)
 {
-    println("А я решил всё поменять ;)")
+    println("Нет, пусть будет как было")
+    var x = Double.MAX_VALUE
+    while(true)x= Math.sqrt(x)
 }
 
 fun Drugoe(x :Double) = x*x
